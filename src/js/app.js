5c012cb36d57ca85f1ec4d45c011f1a6e0e6eafb
Joel Gomez <joel_gomezba@hotmail.com>
2023-05-11 04:41:08 -0600

    Configuracion beta

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,8 @@
 import { menuToggle } from './menu/menu.js'
-import { labelColor } from './config/config.js'
+import { labelColor, newProduct } from './config/config.js'
 
 window.addEventListener('DOMContentLoaded', () => {
 	menuToggle()
 	labelColor()
+	newProduct()
 })
--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,4 +1,23 @@
-import { formInput } from '../selectors/selectors.js'
+const formInput = document.querySelectorAll('.form__input-config')
+const formConfig = document.getElementById('form-config')
+const inputEsquite = Number(document.getElementById('esq'))
+const inputDoriesquite = Number(document.getElementById('de'))
+const inputDoriloco = Number(document.getElementById('dl'))
+const inputTostiloco = Number(document.getElementById('tl'))
+const inputFresas = Number(document.getElementById('fe'))
+const inputGelatina = Number(document.getElementById('ge'))
+const inputEnsalada = Number(document.getElementById('en'))
+const inputBoing = Number(document.getElementById('boing'))
+const inputAguaLitro = Number(document.getElementById('afl'))
+const inputAguaNatural = Number(document.getElementById('anat'))
+const inputPiñada = Number(document.getElementById('pi'))
+const inputNaranjada = Number(document.getElementById('na'))
+const inputLimonada = Number(document.getElementById('li'))
+
+const formProduct = document.getElementById('form-product-new')
+const newProductBtn = document.getElementById('new-product')
+const addProductBtn = document.getElementById('add-product')
+const cancelProductBtn = document.getElementById('cancel-product')
 
 export function labelColor() {
 	formInput.forEach((input) => {
@@ -12,4 +31,51 @@ export function labelColor() {
 			label.classList.remove('label-active')
 		})
 	})
+}
+
+export function newProduct() {
+	function warning() {
+		Swal.fire({
+			title: 'Advertencia!',
+			html:
+				'Al momento de agregar un nuevo producto se eliminaran las ordenes registradas así como el historial de ventas, <strong style="color:#d1831d">ASEGURATE DE IMPRIMIR LA INFORMACIÓN ANTES DE AGREGAR UN NUEVO PRODUCTO.</strong>',
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Sí, continuar',
+			cancelButtonText: 'Cancelar',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				// acción a realizar si se confirma el modal
+				formProduct.classList.add('is-active')
+			} else if (result.dismiss === Swal.DismissReason.cancel) {
+				// acción a realizar si se cancela el modal
+			}
+		})
+	}
+
+	addProductBtn.addEventListener('click', () => {
+		Swal.fire({
+			icon: 'success',
+			title: '¡Producto agregado!',
+			showConfirmButton: false,
+			timer: 1500,
+		})
+		formProduct.classList.remove('is-active')
+	})
+	cancelProductBtn.addEventListener('click', () => {
+		formProduct.classList.remove('is-active')
+	})
+
+	newProductBtn.addEventListener('click', warning)
+}
+
+function validateConfig() {
+	function validateForm() {
+		// if(){
+		// }
+	}
+
+	function configFunction() {
+		formConfig.addEventListener('submit', validateForm)
+	}
 }
