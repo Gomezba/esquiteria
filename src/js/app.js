6aa9aae96ec3985d085497fcc171b3a4b385296a
Joel Gomez <joel_gomezba@hotmail.com>
2023-05-14 02:57:36 -0600

    Configuración terminada

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 import { menuToggle } from './menu/menu.js'
-import { labelColor, newProduct, showProducts, validateConfig, readingObject } from './config/config.js'
+import { labelColor, newProduct, validateConfig, readingObject, showHtmlProducts } from './config/config.js'
 
 window.addEventListener('DOMContentLoaded', () => {
 	menuToggle()
 	if (window.location.pathname.endsWith('/configuration.html')) {
 		readingObject()
-		showProducts()
 		validateConfig()
 		labelColor()
 		newProduct()
+		showHtmlProducts()
 	}
 })
--- src/js/config/config.js
+++ src/js/config/config.js
@@ -1,15 +1,16 @@
 const formInputProduct = document.querySelectorAll('.form__input-config-product')
 const formInputMain = document.querySelectorAll('.form__input-config-main')
+
 const formConfig = document.getElementById('form-config')
 const btnSaveConfig = document.getElementById('btn-save-config')
-const btnEditConfig = document.querySelectorAll('.btn-edit-config')
-const btnCancelConfig = document.querySelectorAll('.btn-cancel-config')
+// const btnEditConfig = document.querySelectorAll('.btn-edit-config')
+// const btnCancelConfig = document.querySelectorAll('.btn-cancel-config')
 const priceContent = document.getElementById('readPrices')
 
 // Categorys
-const botanas = document.getElementById('botanas-category')
-const postres = document.getElementById('desserts-category')
-const bebidas = document.getElementById('drinks-category')
+const botanasContainer = document.getElementById('botanas-category')
+const postresContainer = document.getElementById('desserts-category')
+const bebidasContainer = document.getElementById('drinks-category')
 
 // new form product
 const formProduct = document.getElementById('form-product-new')
@@ -17,12 +18,32 @@ const newProductBtn = document.getElementById('new-product')
 const cancelProductBtn = document.getElementById('cancel-product')
 const body = document.getElementById('body')
 
+const categoryHtml = Object.freeze({
+	botanas: 'botanas',
+	postres: 'postres',
+	bebidas: 'bebidas',
+})
+
 // Input focus y label
 export function labelColor() {
+	const formInputMain = document.querySelectorAll('.form__input-config-main')
+
 	formInputMain.forEach((input) => {
 		let label
 		input.addEventListener('focus', () => {
 			label = input.parentElement.previousElementSibling
+			label.classList.add('label-active')
+		})
+
+		input.addEventListener('blur', () => {
+			label.classList.remove('label-active')
+		})
+	})
+
+	formInputProduct.forEach((input) => {
+		let label
+		input.addEventListener('focus', () => {
+			label = input.previousElementSibling
 			label.classList.add('label-active')
 		})
 
@@ -35,6 +56,11 @@ export function labelColor() {
 class Product {
 	constructor() {
 		this.products = JSON.parse(localStorage.getItem('products')) ?? []
+	}
+
+	deletedProductObj(id) {
+		this.products = this.products.filter((prod) => prod.id != id)
+		console.log('estoy dentro')
 	}
 }
 
@@ -70,6 +96,79 @@ class Ui {
 			}, 2000)
 		}
 	}
+
+	showProductsHtml() {
+		const fragment = document.createDocumentFragment()
+		productsInstance.products.forEach((prod) => {
+			const { name, category, price, id } = prod
+
+			const formField = document.createElement('DIV')
+			const label = document.createElement('LABEL')
+			const formInputOptions = document.createElement('DIV')
+			const currency = document.createElement('SPAN')
+			const input = document.createElement('INPUT')
+			const btnEdit = document.createElement('BUTTON')
+			const btnCancel = document.createElement('BUTTON')
+			const btnDelete = document.createElement('BUTTON')
+
+			formField.classList.add('form__field')
+			label.classList.add('label')
+			label.textContent = name
+			formInputOptions.classList.add('form__input-options')
+			formInputOptions.dataset.id = id
+			currency.classList.add('currency')
+			currency.textContent = '$'
+			input.classList.add('form__input-config', 'form__input-config-main')
+			input.setAttribute('type', 'number')
+			input.setAttribute('step', '0.01')
+			input.setAttribute('name', name)
+			input.setAttribute('disabled', true)
+			input.setAttribute('title', 'Input de precio')
+			input.dataset.name = name
+			input.dataset.category = category
+			input.dataset.id = id
+			input.value = price
+			btnEdit.classList.add('btn', 'btn-edit', 'btn-edit-config')
+			btnEdit.setAttribute('type', 'button')
+			btnEdit.setAttribute('title', 'Editar')
+			btnEdit.innerHTML = '<img src="/src/assets/icons/edit.svg" alt="Boton editar" />'
+			btnCancel.classList.add('btn', 'btn-cancel', 'btn-cancel-config')
+			btnCancel.setAttribute('type', 'button')
+			btnCancel.setAttribute('title', 'Cancelar edición')
+			btnCancel.setAttribute('disabled', 'true')
+			btnCancel.dataset.id = id
+			btnCancel.innerHTML = '<img src="/src/assets/icons/cancel.svg" alt="Boton cancelar" />'
+			btnDelete.classList.add('btn', 'btn-deleted', 'btn-deleted-config')
+			btnDelete.setAttribute('type', 'button')
+			btnDelete.setAttribute('title', 'Eliminar producto')
+			btnDelete.innerHTML = '<img src="/src/assets/icons/delete.svg" alt="Boton eliminar" />'
+
+			btnDelete.addEventListener('click', (e) => {
+				e.preventDefault()
+				deletedProduct(prod)
+			})
+
+			formInputOptions.append(currency, input, btnEdit, btnCancel, btnDelete)
+			formField.append(label, formInputOptions)
+			fragment.append(formField)
+
+			if (fragment === undefined) {
+				return console.error('Error: el fragmento es nulo')
+			}
+
+			if (category === categoryHtml.botanas) {
+				botanasContainer.append(fragment)
+			}
+
+			if (category === categoryHtml.postres) {
+				postresContainer.append(fragment)
+			}
+
+			if (category === categoryHtml.bebidas) {
+				bebidasContainer.append(fragment)
+			}
+		})
+	}
 }
 
 const productsInstance = new Product()
@@ -78,91 +177,91 @@ const ui = new Ui()
 console.log(productsInstance.products)
 const products = [
 	{
-		name: 'esquite',
+		name: 'Esquite',
 		category: 'botanas',
 		price: 25,
 		id: 11,
 	},
 	{
-		name: 'doriesquite',
+		name: 'Doriesquite',
 		category: 'botanas',
 		price: 45,
 		id: 22,
 	},
 	{
-		name: 'doriloco',
+		name: 'Doriloco',
 		category: 'botanas',
 		price: 35,
 		id: 33,
 	},
 	{
-		name: 'tostiloco',
+		name: 'Tostiloco',
 		category: 'botanas',
 		price: 35,
 		id: 44,
 	},
 	{
-		name: 'fresas',
-		category: 'postre',
+		name: 'Fresas',
+		category: 'postres',
 		price: 45,
 		id: 55,
 	},
 	{
-		name: 'gelatina',
-		category: 'postre',
+		name: 'Gelatina con durazno',
+		category: 'postres',
 		price: 45,
 		id: 66,
 	},
 	{
-		name: 'ensalada',
-		category: 'postre',
+		name: 'Ensalada de manzana',
+		category: 'postres',
 		price: 45,
 		id: 77,
 	},
 	{
-		name: 'boing',
+		name: 'Gomiboing',
 		category: 'bebidas',
 		price: 35,
 		id: 88,
 	},
 	{
-		name: 'aguaFrescaLitro',
+		name: 'Agua fresca 1L',
 		category: 'bebidas',
 		price: 25,
 		id: 99,
 	},
 	{
-		name: 'aguaFrescaMedio',
+		name: 'Agua fresca 1/2',
 		category: 'bebidas',
 		price: 15,
 		id: 10,
 	},
 	{
-		name: 'cocaCola',
+		name: 'Coca cola',
 		category: 'bebidas',
 		price: 22,
 		id: 1111,
 	},
 	{
-		name: 'aguaNaturalMedio',
+		name: 'Agua natural 500ml',
 		category: 'bebidas',
 		price: 10,
 		id: 1212,
 	},
 	{
-		name: 'piñada',
+		name: 'Piñada',
 		category: 'bebidas',
 		price: 35,
 		id: 1313,
 	},
 	{
-		name: 'naranjada',
+		name: 'Naranjada',
 		category: 'bebidas',
 		price: 35,
 		id: 1414,
 	},
 	{
-		name: 'limonada',
+		name: 'Limonada',
 		category: 'bebidas',
 		price: 35,
 		id: 1515,
@@ -174,95 +273,126 @@ function addLocal() {
 
 // ---------------------------------------------------------
 
+function deletedProduct(prod) {
+	const { name, price, id } = prod
+	Swal.fire({
+		title: '¿Deseas eliminar el producto?',
+		html: `<strong>${name}</strong>: $${price}`,
+		icon: 'warning',
+		showCancelButton: true,
+		confirmButtonText: 'Eliminar',
+		cancelButtonText: 'Cancelar',
+	}).then((result) => {
+		if (result.isConfirmed) {
+			// productsInstance.deletedProductObj(id)
+			const existingProducts = JSON.parse(localStorage.getItem('products')) ?? []
+			const newProducts = existingProducts.filter((prod) => prod.id !== id)
+			localStorage.setItem('products', JSON.stringify(newProducts))
+			Swal.fire({
+				icon: 'success',
+				title: '¡Producto eliminado!',
+				showConfirmButton: false,
+				timer: 1500,
+			})
+			setTimeout(() => {
+				location.reload()
+			}, 1510)
+		}
+	})
+}
+
 export function readingObject() {
 	priceContent.addEventListener('click', (e) => {
 		e.preventDefault()
-		if (priceContent.dataset.type === 'loading') {
-			location.reload()
-			addLocal()
-		}
 
-		console.log(productsInstance.products)
-	})
-}
-
-export function showProducts() {
-	productsInstance.products.forEach((product) => {
-		const { name, category, price, id } = product
-		formInputMain.forEach((input) => {
-			if (input.name === name) {
-				const btnCancel = input.nextElementSibling.nextElementSibling
-				input.dataset.category = category
-				input.dataset.name = name
-				input.value = price
-				input.dataset.id = id
-				const formInputOptions = input.closest('.form__input-options')
-				formInputOptions.dataset.id = id
-				btnCancel.dataset.id = id
+		Swal.fire({
+			title: 'Advertencia!',
+			html:
+				'Al cargar los productos que estan establecidos en el sistema, se <strong style="color:red">eliminarán los productos que se han agregado manualmente, así como cualquier edición hecha</strong>.',
+			icon: 'warning',
+			showCancelButton: true,
+			confirmButtonText: 'Entiendo, deseo continuar',
+			cancelButtonText: 'Cancelar',
+		}).then((result) => {
+			if (result.isConfirmed) {
+				location.reload()
+				addLocal()
 			}
 		})
 	})
 }
 
-btnEditConfig.forEach((btn) => {
-	btn.addEventListener('click', () => {
-		btn.setAttribute('disabled', true)
-		const currency = btn.previousElementSibling.previousSibling.previousSibling
-		const input = btn.previousElementSibling
-		Number(input.value)
-		input.removeAttribute('disabled')
-		input.focus()
-		const cancel = btn.nextElementSibling
-		cancel.removeAttribute('disabled')
-		currency.classList.add('currency-black')
+export function eventUpdate() {
+	const btnEditConfig = document.querySelectorAll('.btn-edit-config')
 
-		input.addEventListener('input', () => {
-			const moneyRegex = /^(?!0[0-9])[0-9]*(\.[0-9]+)?$/
-			const id = input.dataset.id
+	btnEditConfig.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			btn.setAttribute('disabled', true)
+			const currency = btn.previousElementSibling.previousSibling
+			const input = btn.previousElementSibling
 			Number(input.value)
-			let allInputsValid = true
-			if (input.value.trim() === '') {
-				ui.showAlert('Campo obligatorio', id, 'config')
-				btnSaveConfig.setAttribute('disabled', true)
-				allInputsValid = false
-			} else if (isNaN(Number(input.value)) || !moneyRegex.test(input.value) || input.value <= 0.99) {
-				ui.showAlert('Precio incorrecto', id, 'config')
-				btnSaveConfig.setAttribute('disabled', true)
-				allInputsValid = false
-			}
+			input.removeAttribute('disabled')
+			input.focus()
+			const cancel = btn.nextElementSibling
+			cancel.removeAttribute('disabled')
+			currency.classList.add('currency-black')
 
-			if (allInputsValid) {
-				btnSaveConfig.removeAttribute('disabled')
-			}
+			input.addEventListener('input', () => {
+				const moneyRegex = /^(?!0[0-9])[0-9]*(\.[0-9]+)?$/
+				const id = input.dataset.id
+				Number(input.value)
+				let allInputsValid = true
+				if (input.value.trim() === '') {
+					ui.showAlert('Campo obligatorio', id, 'config')
+					btnSaveConfig.setAttribute('disabled', true)
+					allInputsValid = false
+				} else if (isNaN(Number(input.value)) || !moneyRegex.test(input.value) || input.value <= 0.99) {
+					ui.showAlert('Precio incorrecto', id, 'config')
+					btnSaveConfig.setAttribute('disabled', true)
+					allInputsValid = false
+				}
+
+				if (allInputsValid) {
+					btnSaveConfig.removeAttribute('disabled')
+				}
+			})
 		})
 	})
-})
+}
 
-btnCancelConfig.forEach((btn) => {
-	btn.addEventListener('click', () => {
-		const currency = btn.previousElementSibling.previousSibling.previousSibling.previousSibling.previousSibling
-		const edit = btn.previousElementSibling
-		const input = btn.previousElementSibling.previousElementSibling
-		const idCancel = btn.dataset.id
-		btn.setAttribute('disabled', true)
-		edit.removeAttribute('disabled')
-		input.setAttribute('disabled', true)
-		btnSaveConfig.setAttribute('disabled', true)
-		currency.classList.remove('currency-black')
-		formInputMain.forEach((input) => {
-			if (idCancel === input.dataset.id) {
-				const product = (input.value = productsInstance.products.find((product) => product.id == idCancel))
-				const { price } = product
-				input.value = price
-			}
+export function eventCancel() {
+	const formInputMain = document.querySelectorAll('.form__input-config-main')
+	const btnCancelConfig = document.querySelectorAll('.btn-cancel-config')
+
+	btnCancelConfig.forEach((btn) => {
+		btn.addEventListener('click', () => {
+			const currency = btn.previousElementSibling.previousSibling.previousSibling
+			const edit = btn.previousElementSibling
+			const input = btn.previousElementSibling.previousElementSibling
+			const idCancel = btn.dataset.id
+			btn.setAttribute('disabled', true)
+			edit.removeAttribute('disabled')
+			input.setAttribute('disabled', true)
+			btnSaveConfig.setAttribute('disabled', true)
+			currency.classList.remove('currency-black')
+			formInputMain.forEach((input) => {
+				if (idCancel === input.dataset.id) {
+					const product = (input.value = productsInstance.products.find((product) => product.id == idCancel))
+					const { price } = product
+					input.value = price
+				}
+			})
 		})
 	})
-})
+}
+
 // --------------------------------------------
 
 // TODO formulario principal
 function validateForm(e) {
 	e.preventDefault()
+
+	const formInputMain = document.querySelectorAll('.form__input-config-main')
 
 	let validate = true
 	let newPrice = []
@@ -305,52 +435,6 @@ export function validateConfig() {
 }
 
 //TODO Formulario de nuevos productos
-
-function labelColorProduct() {
-	formInputProduct.forEach((input) => {
-		let label
-		input.addEventListener('focus', () => {
-			label = input.previousElementSibling
-			label.classList.add('label-active')
-		})
-
-		input.addEventListener('blur', () => {
-			label.classList.remove('label-active')
-		})
-	})
-}
-
-function validateProduct(e) {
-	e.preventDefault()
-	const productName = document.getElementById('product-name').value.trim()
-	const productPrice = document.getElementById('product-price').value
-	const selectCategory = document.getElementById('select-category').value
-	const moneyRegex = /^(?!0[0-9])[0-9]*(\.[0-9]+)?$/
-
-	// 	botanas-category
-	// postres-category
-	// bebidas-category
-	if (productName === '' || productPrice === '') {
-		ui.showAlert('Todos los campos son obligatorios', null, 'product')
-		return
-	}
-
-	if (isNaN(productPrice) || !moneyRegex.test(productPrice) || productPrice <= 0.99) {
-		ui.showAlert('Precio incorrecto', null, 'product')
-		return
-	}
-
-	formProduct.reset()
-
-	Swal.fire({
-		icon: 'success',
-		title: '¡Producto agregado!',
-		showConfirmButton: false,
-		timer: 1500,
-	})
-	formProduct.classList.remove('is-active')
-}
-
 export function newProduct() {
 	function warning() {
 		Swal.fire({
@@ -364,7 +448,6 @@ export function newProduct() {
 		}).then((result) => {
 			if (result.isConfirmed) {
 				formProduct.classList.add('is-active')
-				labelColorProduct()
 			}
 		})
 	}
@@ -376,4 +459,64 @@ export function newProduct() {
 	})
 
 	newProductBtn.addEventListener('click', warning)
+}
+
+const newProducto = {
+	name: '',
+	category: '',
+	price: '',
+}
+
+function validateProduct(e) {
+	e.preventDefault()
+	const productName = document.getElementById('product-name').value.trim()
+	const productPrice = document.getElementById('product-price').value
+	const selectCategory = document.getElementById('select-category').value
+	const moneyRegex = /^(?!0[0-9])[0-9]*(\.[0-9]+)?$/
+
+	if (productName === '' || productPrice === '') {
+		ui.showAlert('Todos los campos son obligatorios', null, 'product')
+		return
+	}
+
+	if (isNaN(productPrice) || !moneyRegex.test(productPrice) || productPrice <= 0.99) {
+		ui.showAlert('Precio incorrecto', null, 'product')
+		return
+	}
+
+	newProducto.name = productName
+	newProducto.category = selectCategory
+	newProducto.price = productPrice
+	newProducto.id = Date.now()
+
+	const existingProducts = JSON.parse(localStorage.getItem('products')) ?? []
+	existingProducts.push(newProducto)
+	localStorage.setItem('products', JSON.stringify(existingProducts))
+
+	formProduct.reset()
+
+	Swal.fire({
+		icon: 'success',
+		title: '¡Producto agregado!',
+		showConfirmButton: false,
+		timer: 1500,
+	})
+	formProduct.classList.remove('is-active')
+
+	setTimeout(() => {
+		location.reload()
+	}, 1510)
+}
+
+addEventListener('DOMContentLoaded', () => {
+	setTimeout(() => {
+		eventUpdate()
+		eventCancel()
+	}, 2000)
+})
+
+export function showHtmlProducts() {
+	if (productsInstance.products) {
+		ui.showProductsHtml()
+	}
 }
